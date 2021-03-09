7fb0ee0d0f95615b96b7dd3216358679e7d5185b
Corey Hemminger <hemminger@hotmail.com>
2021-03-08 23:11:09 -0600

    kitchen test fix
    Signed-off-by: Corey Hemminger <hemminger@hotmail.com>

--- test/cookbooks/test/recipes/server_runas_root.rb
+++ test/cookbooks/test/recipes/server_runas_root.rb
@@ -2,6 +2,11 @@ splunk_server_install 'default' do
   svc_user 'root'
 end
 
+append_if_no_line 'Disable file locking check by Splunk startup' do
+  line 'OPTIMISTIC_ABOUT_FILE_LOCKING=1'
+  path "#{splunk_dir}/etc/splunk-launch.conf"
+end
+
 splunk_service 'Splunkd' do
   splunk_bin_path '/opt/splunk/bin/splunk'
   svc_user 'root'
--- test/cookbooks/test/recipes/server_runas_splunk.rb
+++ test/cookbooks/test/recipes/server_runas_splunk.rb
@@ -1,5 +1,10 @@
 splunk_server_install 'default'
 
+append_if_no_line 'Disable file locking check by Splunk startup' do
+  line 'OPTIMISTIC_ABOUT_FILE_LOCKING=1'
+  path "#{splunk_dir}/etc/splunk-launch.conf"
+end
+
 splunk_service 'Splunkd' do
   splunk_bin_path '/opt/splunk/bin/splunk'
 end
